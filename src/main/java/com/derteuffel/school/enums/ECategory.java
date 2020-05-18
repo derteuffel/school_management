484ef12d4e8401047036584a66eb789e21df1d42
derteuffel <derteuffel0@gmail.com>
2020-05-18 09:03:12 +0100

    make changes in encadrement login and activation module

--- src/main/java/com/derteuffel/school/enums/ECategory.java
+++ src/main/java/com/derteuffel/school/enums/ECategory.java
@@ -4,5 +4,6 @@ public enum ECategory {
     expert_yesb_primaire,
     expert_yesb_secondaire,
     appui_redaction_travail_de_fin_de_cycle,
-    expert_yesb_en_formation_professionnelle
+    expert_yesb_en_formation_professionnelle,
+    expert_yesb_en_stage_professionnelle
 }
